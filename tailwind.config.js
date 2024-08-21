04db5783f2d2c42354ef45ad3a35daea877ae38c
sharoj57 <sharojcse@gmail.com>
2024-08-21 21:39:28 +0600

    added this part

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,4 @@ module.exports = {
 
 
 
-  <div class="flex flex-col md:flex-row lg:flex-row p-12 lg:p-24"> 
-  <!-- Text Content -->
-  <div class="space-y-6 flex-1">
-      <h2 class="font-bold text-6xl">Ride-on R15 V4 <br>with Smile</h2>
-      <p class="font-light text-[#000000CC]">Amet minim mollit non deserunt ullamco 
-        est sit aliqua dolor do amet sint. Velit officia consequat <br>duis enim velit mollit.</p>
-        
-          <a class="btn bg-bike-primary text-white w-40 rounded-md">Purchase</a></div>
-         <!-- Image Content -->
-          <div class="flex-1 ">
-            <img
-            src="/slider/bike1.png"
-            class="w-[406px]" />
-          </div>
-          </div>
+ 
